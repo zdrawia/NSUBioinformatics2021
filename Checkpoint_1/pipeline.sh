b939e14586f4ab0f5a84ac29fff660954a5049e8
Vladimir <asteropheum@gmail.com>
2021-10-18 18:01:09 +0700

    Update pipeline.sh

--- Checkpoint_1/pipeline.sh
+++ Checkpoint_1/pipeline.sh
@@ -3,8 +3,8 @@
 fastqc $1.fastq.gz
 mv $1_fastqc.html $1.html
 rm $1_fastqc.zip
-minimap2/minimap2 -d ref.mmi ref.fa
-minimap2/minimap2 -a ref.mmi $1.fastq.gz > $1.sam
+minimap2 -d ref.mmi ref.fa
+minimap2 -a ref.mmi $1.fastq.gz > $1.sam
 samtools flagstat $1.sam > $1_flagstat.txt
 PY_RES=$(python script.py $1_flagstat.txt)
 if (( $(echo "$PY_RES > 0.9" | bc -l) ));
